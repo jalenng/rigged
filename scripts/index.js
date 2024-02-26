8e3a5603b7b3465511a1cefb7b74672541a74576
jalenng <jalen_ng@outlook.com>
2024-02-25 17:03:15 -0800

    refactor

--- scripts/index.js
+++ scripts/index.js
@@ -2,72 +2,99 @@
 document.addEventListener("DOMContentLoaded", function () {
   const entityManager = new EntityManager();
 
-  // show scissors
-  entityManager.add(new Entity("scissors", "scissors.png", { x: 10, y: 10 }));
+  function spawnScissors() {
+    entityManager.add(new Entity("scissors", "scissors.png", { x: 10, y: 10 }));
+  }
 
-  entityManager.add(
-    // show package
-    new Entity("package", "package.png", { x: 300, y: 180 }, ({ from, to }) => {
-      // scissors --> package
-      if (from === "scissors") {
-        console.log(entityManager.getEntity("package"));
-        entityManager.getEntity(from).remove();
-        entityManager.getEntity(to).remove();
-        ripSound.play();
-        
-        // show fire extinguisher after delay
-        setTimeout(() => {
-          entityManager.add(
-            new Entity("fireExtinguisher", "fireExtinguisher.png", {
-              x: 500,
-              y: 50,
-            })
-          );
-        }, 1000);
+  function spawnPackage() {
+    entityManager.add(
+      // show package
+      new Entity(
+        "package",
+        "package.png",
+        { x: 300, y: 180 },
+        ({ from, to }) => {
+          // scissors --> package
+          if (from === "scissors") {
+            console.log(entityManager.getEntity("package"));
+            entityManager.getEntity(from).remove();
+            entityManager.getEntity(to).remove();
+            ripSound.play();
 
-        // show bomb
-        entityManager.add(
-          new Entity("bomb", "bomb.png", { x: 300, y: 180 }, ({ from, to }) => {
-            if (from === "fireExtinguisher") {
-              // fire extinguisher --> bomb
-              entityManager.getEntity(from).remove();
-              entityManager.getEntity(to).remove();
+            // show fire extinguisher after delay
+            setTimeout(spawnFireExtinguisher, 1000);
 
-              // show oven after delay
-              setTimeout(() => {
-                entityManager.add(
-                  new Entity(
-                    "oven",
-                    "oven.png",
-                    {
-                      x: 420,
-                      y: 69,
-                    },
-                    ({ from, to }) => {
-                      if (from === "ice") {
-                        entityManager.getEntity(from).remove();
-                        entityManager.getEntity(to).remove();
+            // show bomb
+            spawnBomb()
+          }
+        }
+      )
+    );
+  }
 
-                        // show cooked duck
-                        entityManager.add(
-                          new Entity("cookedDuck", "cookedDuck.png", { x: 420, y: 69 })
-                        );
-                      }
-                    }
-                  )
-                );
-              }, 1000);
+  function spawnFireExtinguisher() {
+    entityManager.add(
+      new Entity("fireExtinguisher", "fireExtinguisher.png", {
+        x: 500,
+        y: 50,
+      })
+    );
+  }
 
-              // show ice
-              entityManager.add(
-                new Entity("ice", "Ice.png", { x: 300, y: 180 })
-              );
-            }
-          })
-        );
-      }
-    })
-  );
+  function spawnOven() {
+    entityManager.add(
+      new Entity(
+        "oven",
+        "oven.png",
+        {
+          x: 420,
+          y: 69,
+        },
+        ({ from, to }) => {
+          if (from === "ice") {
+            entityManager.getEntity(from).remove();
+            entityManager.getEntity(to).remove();
 
-  showEnding(entityManager, Entity);
+            // show cooked duck
+            spawnCookedDuck();
+          }
+        }
+      )
+    );
+  }
+
+  function spawnBomb() {
+    entityManager.add(
+      new Entity("bomb", "bomb.png", { x: 300, y: 180 }, ({ from, to }) => {
+        if (from === "fireExtinguisher") {
+          // fire extinguisher --> bomb
+          entityManager.getEntity(from).remove();
+          entityManager.getEntity(to).remove();
+
+          // show oven after delay
+          setTimeout(spawnOven, 1000);
+
+          // show ice
+          spawnIce();
+        }
+      })
+    );
+  }
+
+  function spawnIce() {
+    entityManager.add(new Entity("ice", "Ice.png", { x: 300, y: 180 }));
+  }
+
+  function spawnCookedDuck() {
+    entityManager.add(
+      new Entity("cookedDuck", "cookedDuck.png", {
+        x: 420,
+        y: 69,
+      })
+    );
+  }
+
+  // START
+  setTimeout(spawnScissors, 1000);
+  spawnPackage();
 });
