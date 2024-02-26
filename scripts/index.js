1756cbc5ad189f15ecebb64a2d437dd09574c10c
jalenng <jalen_ng@outlook.com>
2024-02-25 16:40:38 -0800

    add combos until cooked duck

--- scripts/index.js
+++ scripts/index.js
@@ -2,16 +2,69 @@
 document.addEventListener("DOMContentLoaded", function () {
   const entityManager = new EntityManager();
 
-  entityManager.add(new Entity("scissors", "Scissors.png", { x: 10, y: 10 }));
+  // show scissors
+  entityManager.add(new Entity("scissors", "scissors.png", { x: 10, y: 10 }));
 
   entityManager.add(
-    new Entity("package", "Package.png", { x: 300, y: 300 }, ({ from }) => {
+    // show package
+    new Entity("package", "package.png", { x: 300, y: 180 }, ({ from, to }) => {
+      // scissors --> package
       if (from === "scissors") {
         console.log(entityManager.getEntity("package"));
-        entityManager.getEntity("package").remove();
-        entityManager.getEntity("scissors").remove();
-        entityManager.add(new Entity("bomb", "Bomb.png", { x: 210, y: 210 }));
+        entityManager.getEntity(from).remove();
+        entityManager.getEntity(to).remove();
         ripSound.play();
+        
+        // show fire extinguisher after delay
+        setTimeout(() => {
+          entityManager.add(
+            new Entity("fireExtinguisher", "fireExtinguisher.png", {
+              x: 500,
+              y: 50,
+            })
+          );
+        }, 1000);
+
+        // show bomb
+        entityManager.add(
+          new Entity("bomb", "bomb.png", { x: 300, y: 180 }, ({ from, to }) => {
+            if (from === "fireExtinguisher") {
+              // fire extinguisher --> bomb
+              entityManager.getEntity(from).remove();
+              entityManager.getEntity(to).remove();
+
+              // show oven after delay
+              setTimeout(() => {
+                entityManager.add(
+                  new Entity(
+                    "oven",
+                    "oven.png",
+                    {
+                      x: 420,
+                      y: 69,
+                    },
+                    ({ from, to }) => {
+                      if (from === "ice") {
+                        entityManager.getEntity(from).remove();
+                        entityManager.getEntity(to).remove();
+
+                        // show cooked duck
+                        entityManager.add(
+                          new Entity("cookedDuck", "cookedDuck.png", { x: 420, y: 69 })
+                        );
+                      }
+                    }
+                  )
+                );
+              }, 1000);
+
+              // show ice
+              entityManager.add(
+                new Entity("ice", "Ice.png", { x: 300, y: 180 })
+              );
+            }
+          })
+        );
       }
     })
   );
